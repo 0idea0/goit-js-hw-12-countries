2b590c0341cfa5e970608d1178b5716332ec611b
KYRYLO VOITOVSKYI <kirill.voitovskiy@gmail.com>
2021-10-19 22:13:57 +0300

    add countries search

---
+++ src/js/pnotify.js
@@ -0,0 +1,28 @@
+import { error, info } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+export function onNoCountry() {
+  info({
+    title: 'Ошибка!',
+    text: 'Введите корректный запрос ...(Ukraine)',
+    delay: 2000,
+    closerHover: true,
+    mouseReset: true,
+    shadow: true,
+  });
+}
+
+export function onOutputInfo() {
+  error({
+    title: 'Большое количество совпадений',
+    text: 'Пожалуйта, введите более конкретный запрос ...(USA)',
+    delay: 2000,
+    closerHover: true,
+    mouseReset: true,
+    shadow: true,
+  });
+}
+export function onError() {
+  alert('Данные для поиска страны отстутсву');
+}
